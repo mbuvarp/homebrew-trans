f0d9b670bd97ba021ff45599e1b79ff304175c18
Magnus Buvarp <magnus.buvarp@gmail.com>
2026-01-28 17:34:53 +0100

    chore: scaffold tap
    Created by codex

---
+++ Formula/trans.rb
@@ -0,0 +1,16 @@
+class Trans < Formula
+  desc "Translation utility for react-intl JSON files"
+  homepage "https://github.com/mbuvarp/trans"
+  url "https://github.com/mbuvarp/trans/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "REPLACE_WITH_SHA256"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/trans", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-trans
+
+Homebrew tap for the `trans` CLI.
+
+## Install
+
+```bash
+brew tap mbuvarp/trans
+brew install trans
+```
+
+## Update formula
+
+1. Create a new release in https://github.com/mbuvarp/trans (tagged like `v0.1.0`).
+2. Update `Formula/trans.rb` with the new `url` and `sha256`.
+3. Commit and push to this tap repo.
