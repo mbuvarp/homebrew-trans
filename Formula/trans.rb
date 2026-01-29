d95901ca76febdedb39427b27cb16987a9d8d3de
mbuvarp <7132248+mbuvarp@users.noreply.github.com>
2026-01-29 18:57:11 +0000

    chore: update trans formula for v0.1.4

--- Formula/trans.rb
+++ Formula/trans.rb
@@ -2,10 +2,22 @@ class Trans < Formula
   desc "Translation utility for react-intl JSON files"
   homepage "https://github.com/mbuvarp/trans"
   url "https://github.com/mbuvarp/trans/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "73450c9a40e8a999b6a6713d18dba79e3aafda8837542636723185e70066c9d8"
+  sha256 "b3d995f73fbfad8d8e9c76797a025c4a8023b5040b0c15cbc76d25932df3daf6"
+  bottle do
+    root_url "https://github.com/mbuvarp/trans/releases/download/v0.1.4"
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 arm64_sequoia: "683d677bb1ca72ef5e84ac66cb460e78406b981c10c272b4b9276226f8d27044"
+  end
   bottle do
     root_url "https://github.com/mbuvarp/trans/releases/download/v0.1.4"
     sha256 arm64_sequoia: "ef7aab4e75ebb93ab49a26ce6ea64f68a8a1a832b18c900b028d95c185b757bf"
+  bottle do
+    root_url "https://github.com/mbuvarp/trans/releases/download/v0.1.4"
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 arm64_sequoia: "683d677bb1ca72ef5e84ac66cb460e78406b981c10c272b4b9276226f8d27044"
+  end
   end
 
   depends_on "rust" => :build
