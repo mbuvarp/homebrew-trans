605eaeb2341baab96315a9bab40168caf8031f16
Magnus Buvarp <magnus.buvarp@gmail.com>
2026-07-14 00:15:52 +0200

    ci: add multi-platform bottle workflows
    Created by codex

--- Formula/trans.rb
+++ Formula/trans.rb
@@ -3,7 +3,6 @@ class Trans < Formula
   homepage "https://github.com/mbuvarp/trans"
   url "https://github.com/mbuvarp/trans/archive/refs/tags/v0.6.0.tar.gz"
   sha256 "3fe4a244d774042ed7c36f2fc450b4655c34eafdfe9c10a6fcdc1dcc06e2de72"
-  # Bottle metadata removed; source-only installs are supported.
 
   depends_on "rust" => :build
 
