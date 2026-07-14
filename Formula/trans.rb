e2ece3a59b7c2fbea70dda66bddeda0d00e0f624
Magnus Buvarp <magnus.buvarp@gmail.com>
2026-07-14 08:45:32 +0000

    trans: update 0.7.1 bottle.

--- Formula/trans.rb
+++ Formula/trans.rb
@@ -5,10 +5,10 @@ class Trans < Formula
   sha256 "99ed36ebab8447dd3901b74e5f9ee9115cf9f4f2a9a0ff955be56d20866dd108"
 
   bottle do
-    root_url "https://github.com/mbuvarp/homebrew-trans/releases/download/trans-0.7.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "98cf89abf88992a99fab817e50465a50444fd9138b844e4a6198b1383dd994f7"
-    sha256 cellar: :any_skip_relocation, sequoia:      "295f51e965eeb8a7051fc04d0a80e81620bdd5c5b70c307ace5c86335830d851"
-    sha256 cellar: :any,                 x86_64_linux: "38576a03df895867295faf43df9cc1402a205385f4b980bbf836812d3a957baa"
+    root_url "https://github.com/mbuvarp/homebrew-trans/releases/download/trans-0.7.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "45d083738a37ae76e2360f7263016b6f4e83600aa29c7c9cab7e90b042aa5f99"
+    sha256 cellar: :any_skip_relocation, sequoia:      "23c1a8a3a9ac97a3213c03d8477eaf9a3bc2b6861dec84b0b59a978b0953f3b8"
+    sha256 cellar: :any,                 x86_64_linux: "81b265f8e30d4c3492ac5dacbace0261ae36c72052ac452e62f6131a9d73a9d4"
   end
 
   depends_on "rust" => :build
